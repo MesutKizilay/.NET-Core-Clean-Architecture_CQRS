04b80ff40f143c26c080ef70d5b847114d080358
MesutKizilay <mesutkizilay3868@gmail.com>
2023-09-16 22:27:57 +0300

    Architecture was created.

---
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class CreateBrandCommand:IRequest<CreatedCommandResponse>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedCommandResponse>
+        {
+            public Task<CreatedCommandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
