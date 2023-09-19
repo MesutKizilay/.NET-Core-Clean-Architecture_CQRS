e3577cfa72afb713ffa6ba9a67b3d109c5960361
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-09-19 22:59:00 +0300

    Paging implementasion was added.

--- src/corePackages/Core.Packages/Core.Persistence/Repositories/IAsyncRepository.cs
+++ src/corePackages/Core.Packages/Core.Persistence/Repositories/IAsyncRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
--- src/corePackages/Core.Packages/Core.Persistence/Repositories/IRepository.cs
+++ src/corePackages/Core.Packages/Core.Persistence/Repositories/IRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
 namespace Core.Persistence.Repositories
