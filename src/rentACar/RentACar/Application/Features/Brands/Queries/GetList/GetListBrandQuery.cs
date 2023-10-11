256173d494da09b442654aecafeda18130daa96b
MesutKizilay <mesutkizilay3868@gmail.com>
2023-10-11 17:31:33 +0300

    Transaction coonfiguration was added.

--- src/rentACar/RentACar/Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
+++ src/rentACar/RentACar/Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Caching;
 using Core.Application.Requests;
 using Core.Application.Responses;
 using Core.Persistence.Paging;
@@ -8,7 +9,7 @@ using MediatR;
 
 namespace Application.Features.Brands.Queries.GetList
 {
-    public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>
+    public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>,ICachableRequest
     {
         public PageRequest PageRequest { get; set; }
 
